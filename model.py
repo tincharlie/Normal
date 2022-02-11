876bb60b2e373c5966669984bb9cc29cef9d7666
Vipul Shukla <84698140+tincharlie@users.noreply.github.com>
2022-02-11 11:40:30 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from pickle import dump, load
 from pandas import read_csv
 
 A = read_csv("/Users/user/Desktop/python/datasets/50_Startups.csv")
-X = A[["RND", "MKT"]]
+X = A[["RND"]]
 Y = A[["PROFIT"]]
 from sklearn.linear_model import LinearRegression
 
